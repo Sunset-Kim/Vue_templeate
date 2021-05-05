8f8f7fdc377b6ff9d13e7cc33f2ce706ae1ded75
Sunset-Kim <wood2002@naver.com>
2021-05-06 02:26:56 +0900

    ✨ Section Team + label style

---
+++ src/components/AppTeam.vue
@@ -0,0 +1,352 @@
+<template>
+    <div id="app-team">
+        <div class="title-group">
+            <div class="title-top">Meet Our Team</div>
+            <div class="title-main">We’ve Most Experience</div>
+            <div class="title-sub">Team Members</div>
+        </div>
+        <div class="wrap">
+            <div class="team-container">
+                <div class="team-item">
+                    <div class="team-img">
+                        <img src="" alt="" />
+                        <span class="label-mail">
+                            <i class="far fa-envelope-open"></i>
+                        </span>
+                    </div>
+
+                    <div class="team-info">
+                        <h6>Web Designer</h6>
+                        <h4>David SL Warner</h4>
+                    </div>
+
+                    <div class="btn-plus">
+                        <i class="fas fa-plus"></i>
+                    </div>
+
+                    <ul class="team-sns-group">
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-twitter"></i>
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-youtube"></i>
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-behance"></i>
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="team-item">
+                    <div class="team-img">
+                        <img src="" alt="" />
+                        <span class="label-mail">
+                            <i class="far fa-envelope-open"></i>
+                        </span>
+                    </div>
+
+                    <div class="team-info">
+                        <h6>Web Designer</h6>
+                        <h4>David SL Warner</h4>
+                    </div>
+
+                    <div class="btn-plus">
+                        <i class="fas fa-plus"></i>
+                    </div>
+
+                    <ul class="team-sns-group">
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-twitter"></i>
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-youtube"></i>
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-behance"></i>
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="team-item">
+                    <div class="team-img">
+                        <img src="" alt="" />
+                        <span class="label-mail">
+                            <i class="far fa-envelope-open"></i>
+                        </span>
+                    </div>
+
+                    <div class="team-info">
+                        <h6>Web Designer</h6>
+                        <h4>David SL Warner</h4>
+                    </div>
+
+                    <div class="btn-plus">
+                        <i class="fas fa-plus"></i>
+                    </div>
+
+                    <ul class="team-sns-group">
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-twitter"></i>
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-youtube"></i>
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-behance"></i>
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="team-item">
+                    <div class="team-img">
+                        <img src="" alt="" />
+                        <span class="label-mail">
+                            <i class="far fa-envelope-open"></i>
+                        </span>
+                    </div>
+
+                    <div class="team-info">
+                        <h6>Web Designer</h6>
+                        <h4>David SL Warner</h4>
+                    </div>
+
+                    <div class="btn-plus">
+                        <i class="fas fa-plus"></i>
+                    </div>
+
+                    <ul class="team-sns-group">
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-twitter"></i>
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-youtube"></i>
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <i class="fab fa-behance"></i>
+                                <i class="fab fa-behance"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#app-team {
+    margin-bottom: 13rem;
+}
+.team-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.team-item {
+    position: relative;
+    width: 270px;
+    padding: 4rem;
+    text-align: center;
+    border: 1px solid #e9e9e9;
+    overflow: hidden;
+    margin-right: 3rem;
+    margin-bottom: 3rem;
+}
+.team-item:last-child {
+    margin-right: 0;
+}
+
+.team-item::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: auto;
+    left: 0;
+    right: 0;
+    height: 0%;
+    background: var(--primary);
+    z-index: -1;
+    transition: 0.3s ease-in-out;
+}
+.team-img {
+    position: relative;
+    background: var(--text-dark);
+    width: 170px;
+    height: 170px;
+    margin: 0 auto;
+    margin-bottom: 3rem;
+    border-radius: 50%;
+    /* overflow: hidden; */
+}
+.team-img img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+}
+.label-mail {
+    position: absolute;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    bottom: 0;
+    right: 0;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: var(--secondary);
+}
+.team-info {
+    margin-bottom: 2.5rem;
+}
+.team-info h6 {
+    color: var(--primary);
+    font-size: 1.5rem;
+    border-width: 600;
+    margin-bottom: 1.5rem;
+    transition: 0.3s ease-in-out;
+}
+.team-info h4 {
+    font-size: 2rem;
+    font-weight: 900;
+    transition: 0.3s ease-in-out;
+}
+.btn-plus {
+    position: relative;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 35px;
+    height: 35px;
+    border: 1px solid #e9e9e9;
+    font-size: 1.2rem;
+    color: var(--primary);
+    z-index: -2;
+    opacity: 1;
+    transition: 0.2s;
+}
+.team-sns-group {
+    position: absolute;
+    /* bottom: calc(4rem); */
+    bottom: -4rem;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    transition: 0.3s ease-in-out;
+    /* opacity: 0; */
+}
+.team-sns-group li a {
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    background: var(--secondary);
+    margin-right: 1rem;
+    overflow: hidden;
+}
+.team-sns-group li a:hover i {
+    transform: translateY(-100%);
+}
+.team-sns-group li a:hover i:first-child {
+    opacity: 0;
+}
+.team-sns-group li a:hover i:last-child {
+    opacity: 1;
+}
+.team-sns-group li a i {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+    transition: 0.2s ease-in;
+}
+.team-sns-group li a i:last-child {
+    color: var(--text-light);
+    opacity: 0;
+}
+.team-sns-group li:last-child a {
+    margin-right: 0;
+}
+/* hover */
+.team-item:hover::before {
+    height: 100%;
+}
+.team-item:hover .team-info {
+    color: var(--text-light);
+}
+.team-item:hover .team-info h6 {
+    color: var(--text-light);
+}
+.team-item:hover .team-sns-group {
+    bottom: 4rem;
+    opacity: 1;
+}
+.team-item:hover .btn-plus {
+    opacity: 0;
+}
+</style>
