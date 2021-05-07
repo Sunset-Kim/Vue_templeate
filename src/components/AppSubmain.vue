53266638806a2f1d460057b6fefeb6b534f85e84
Sunset-Kim <wood2002@naver.com>
2021-05-07 16:24:45 +0900

    ✨ New component for AppSubmain

---
+++ src/components/AppSubmain.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app-submain">
+      <div class="submain-img">
+          <slot></slot>
+      </div>
+      <div class="submain-text">
+          <slot name="title">About Us</slot>
+          <slot name="subtitle">Home > <span>About Us</span></slot>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+           
+        }
+    },
+    
+}
+</script>
+
+<style>
+#app-submain {
+    position: relative;
+    height: 52.5rem;
+}
+.submain-img {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #000;
+}
+.submain-img::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #08133b;
+    opacity: 0.4;
+    z-index: 1;
+}
+.submain-img img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+}
+.submain-text {
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    color: white;
+    z-index: 99;
+}
+.submain-text h1 {
+    font-size: 8.5rem;
+    line-height: 9rem;
+    margin-bottom: 3rem;
+}
+.submain-text h2 {
+    font-size: 2.4rem;
+}
+.submain-text span {
+    color: var(--secondary);
+}
+</style>
