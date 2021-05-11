026f63ebd84ae27c5c15e5e87e0a645109501009
Sunset-Kim <wood2002@naver.com>
2021-05-11 14:27:38 +0900

    📱 tap btn group responsive design

--- src/components/AppTabmenu.vue
+++ src/components/AppTabmenu.vue
@@ -5,15 +5,15 @@
           <div class="wrap">
             <div class="row">
 
-              <div class="col-lg-4 col-md-4">
+              <div class="col-lg-4 col-md-4 col-sm-6">
                 <a v-bind:class="{active: menuGroup.menu1}" v-on:click="toggleMenu" class="btn-main-ef btn-primary" href="#none" data-index="1">mission & vision</a>
               </div>
 
-              <div class="col-lg-4 col-md-4">
+              <div class="col-lg-4 col-md-4 col-sm-6">
                 <a v-bind:class="{active: menuGroup.menu2}" v-on:click="toggleMenu" class="btn-main-ef btn-primary" href="#none" data-index="2">our Goal</a>
               </div>
 
-              <div class="col-lg-4 col-md-4">
+              <div class="col-lg-4 col-md-4 col-sm-6">
                 <a v-bind:class="{active: menuGroup.menu3}" v-on:click="toggleMenu" class="btn-main-ef btn-primary" href="#none" data-index="3">Company History</a>
               </div>
 
@@ -28,6 +28,208 @@
         <div class="tapmenu-container">
           <transition name="fade">
             <div v-if="menuGroup.menu1" class="tapmenu-content">
+              <div class="wrap">
+                <div class="row">
+
+                  <div class="col-lg-5 col-md-8 col-sm-6">
+
+                    <div class="content-title">
+                      <h2>Change World</h2>
+                      <h4>Better Future</h4>
+                    </div>
+
+                    <div class="content-main">
+                      <p>
+                        Sed ut perspiciatis unde omnis iste natu voluptatem
+                        accusantium doloremque laudantium totam aperiam
+                        eaque ipsa quaab inventore veritatis quasi architecto
+                        beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                        voluptatem quia voluptas sit aspernatur aut
+                      </p>
+                    </div>
+
+                    <div class="content-list">
+                      <ul>
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            SEO Belong In Your Web Design Process
+                          </span>
+
+                        </li>
+
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            Build Music Manager With Nuxt And Express
+                          </span>
+                          
+                        </li>
+
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            What Does Foldable Web Actually Mean
+                          </span>
+                          
+                        </li>
+
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            How To Build Simple Cryptocurrency
+                          </span>
+                          
+                        </li>
+                      </ul>
+                    </div>
+
+
+                  </div>
+
+                  <div class="col-lg-1 col-md-0"></div>
+
+                  <div class="col-lg-6 col-md-4 col-sm-6">
+
+                    <div class="content-img">
+                      <img src="../assets/header/slide01.jpg" alt="이미지메인">
+
+                      <div class="content-img-sub">
+                        <img src="../assets/best/01.jpg" alt="이미지보조">
+                      </div>
+
+                      <div class="content-img-effect">
+                        <img src="../assets/subpage/content-effect.png" alt="이미지효과">
+                      </div>
+
+                    </div>
+                    
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </transition>
+
+          <transition name="fade">
+            <div v-if="menuGroup.menu2" class="tapmenu-content">
+              <div class="wrap">
+                <div class="row">
+
+                  <div class="col-lg-5 col-md-8 col-sm-6">
+
+                    <div class="content-title">
+                      <h2>Change World</h2>
+                      <h4>Better Future</h4>
+                    </div>
+
+                    <div class="content-main">
+                      <p>
+                        Sed ut perspiciatis unde omnis iste natu voluptatem
+                        accusantium doloremque laudantium totam aperiam
+                        eaque ipsa quaab inventore veritatis quasi architecto
+                        beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                        voluptatem quia voluptas sit aspernatur aut
+                      </p>
+                    </div>
+
+                    <div class="content-list">
+                      <ul>
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            SEO Belong In Your Web Design Process
+                          </span>
+
+                        </li>
+
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            Build Music Manager With Nuxt And Express
+                          </span>
+                          
+                        </li>
+
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            What Does Foldable Web Actually Mean
+                          </span>
+                          
+                        </li>
+
+                        <li>
+
+                          <i class="icon-check">
+                            <i class="fas fa-check"></i>
+                          </i>
+
+                          <span>
+                            How To Build Simple Cryptocurrency
+                          </span>
+                          
+                        </li>
+                      </ul>
+                    </div>
+
+
+                  </div>
+
+                  <div class="col-lg-1 col-md-0"></div>
+
+                  <div class="col-lg-6 col-md-4 col-sm-6">
+
+                    <div class="content-img">
+                      <img src="../assets/header/slide01.jpg" alt="이미지메인">
+
+                      <div class="content-img-sub">
+                        <img src="../assets/best/01.jpg" alt="이미지보조">
+                      </div>
+
+                      <div class="content-img-effect">
+                        <img src="../assets/subpage/content-effect.png" alt="이미지효과">
+                      </div>
+
+                    </div>
+                    
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </transition>
+
+          <transition name="fade">
+            <div v-if="menuGroup.menu3" class="tapmenu-content">
               <div class="wrap">
                 <div class="row">
 
@@ -215,6 +417,16 @@ export default {
 	width: 100%;
 	left: 0px;
 	right: auto
+}
+
+@media screen and (max-width: 768px) {
+
+  .tabmenu-btn-group {
+    transform: translateY(-33.333%);
+  }
+  .tabmenu-btn-group a {
+    margin-bottom: 1rem;
+  }
 }
 /* content */
 .tapmenu-content {
