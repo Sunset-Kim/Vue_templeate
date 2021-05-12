b025786695aaba65346999de2ea9e3bc6d3de342
Sunset-Kim <wood2002@naver.com>
2021-05-12 15:46:26 +0900

    ✨project 세부설명 컴포넌트 구현: 반응형완료

--- src/components/AppSubdesc.vue
+++ src/components/AppSubdesc.vue
@@ -1,9 +1,486 @@
 <template>
-    <div id="app-subdesc"></div>
+    <div id="app-subdesc">
+        <!-- tab menu -->
+        <div class="tab-btn-group">
+            <div class="wrap">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <button type="button" class="btn-tab">
+                            Description
+                        </button>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <button type="button" class="btn-tab">
+                            Updates
+                        </button>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <button type="button" class="btn-tab">
+                            Backer List
+                        </button>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-3">
+                        <button type="button" class="btn-tab">
+                            Reviews
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+        <!-- tab contents-->
+        <div class="tab-contents">
+            <div class="wrap">
+                <div class="row">
+                    <!-- main contents -->
+                    <div class="col-lg-8 col-md-12">
+                        <div class="tab-contents-main">
+                            <h4 class="tab-contents-title">
+                                Best Smart Headphone In 2020
+                            </h4>
+
+                            <p class="tab-contents-desc">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu
+                            </p>
+                            
+                            <div class="tab-contents-img">
+                                <div class="tab-contents-img-wide">
+                                    <img src="" alt="상세이미지 메인">
+                                </div>
+                            </div>
+                            
+
+                            <h4 class="tab-contents-title">
+                                Why Donate Our Products
+                            </h4>
+
+                            <p class="tab-contents-desc">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu
+                            </p>
+
+                            <div class="tab-contents-list">
+                                <ul>
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+                                   
+                                </ul>
+
+                                <ul>
+                                    
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                    <li>
+                                        <div class="icon-check">
+                                            <i class="fas fa-check"></i>
+                                        </div>
+                                        <span>
+                                            Standard Lorem Sum Passage Used
+                                        </span>
+                                    </li>
+
+                                </ul>
+
+                            </div>
+                            
+                            <div class="tab-contents-img">
+                                <div class="tab-contents-img-half">
+                                    <img src="" alt="상세이미지1">
+                                </div>
+
+                                <div class="tab-contents-img-half">
+                                    <img src="" alt="상세이미지2">
+                                </div>
+                            </div>
+                            
+
+                            <p class="tab-contents-desc">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatu
+                            </p>
+                            
+
+
+
+
+
+                        </div>
+                    </div>
+                    <!-- side contents -->
+                    <div class="col-lg-4 col-md-12">
+                        <aside class="tab-contents-side">
+                            <!-- rewards -->
+                            <div class="rewards">
+                                <h4>Rewards</h4>
+                                <div class="rewards-img">
+                                    <img src="" alt="">
+                                </div>
+                                <span class="rewards-info"><strong>258</strong> or More</span>
+                                <p>
+                                    But must explain to you how all this mistaken idea of denouncing plasue and praising pain was born
+                                </p>
+                                <div class="rewards-date">
+                                    <h6>30 DEC 2020</h6>
+                                    Estimated Delivery
+                                </div>
+                                <div class="rewards-icon">
+                                    <ul>
+                                        <li>
+                                            <i class="fas fa-user-friends"></i>
+                                            <span>5 Backers</span>
+                                        </li>
+                                        <li>
+                                            <i class="fas fa-medal"></i>
+                                            <span>29 Rewards Left</span>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <a class="btn-padding btn-secondary btn-main-ef" href="#none">select rewards</a>
+                            </div>
+
+                            <!-- banner -->
+                            <div class="banner">
+                                <div class="banner-subtitle">
+                                    Donate Now
+                                </div>
+                                <div class="banner-maintitle">
+                                    Want To Donate<br>Our Product
+                                </div>
+                                <a class="btn-190 btn-secondary btn-main-ef" href="#none">Donate Now</a>
+                            </div>
+                        </aside>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+#app-subdesc {
+    padding-bottom: 13rem;
+}
+.tab-btn-group {
+    transform: translateY(-50%);
+}
+.tab-btn-group .btn-tab {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 8rem;
+    font-size: 2rem;
+    background: var(--primary);
+    color: white;
+    margin-bottom: 1rem;
+    transition: 0.3s ease-in-out;
+}
+.tab-btn-group .btn-tab:hover {
+    background: var(--secondary);
+    color: var(--text-main);
+}
+/* main content */
+.tab-contents-title {
+    font-size: 3rem;
+    font-weight: 600;
+    margin-bottom: 2.5rem;
+}
+.tab-contents-desc {
+    font-size: 1.5rem;
+    line-height: 2.8rem;
+    color: var(--text-sub);
+}
+.tab-contents-img {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 5rem 0;
+}
+.tab-contents-img-wide {
+    width: 100%;
+    height: 44.5rem;
+    background: #000;
+}
+.tab-contents-img-wide img {
+    display: block;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+}
+.tab-contents-list {
+    display: flex;
+    margin: 5rem 0;
+}
+.tab-contents-list ul {
+    width: 50%;
+    padding-right: 2rem;
+}
+.tab-contents-list ul li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 2rem;
+}
+.tab-contents-list ul li:hover .icon-check {
+    background: var(--primary);
+    color: white;
+    border-color: var(--primary);
+}
+.tab-contents-list ul li .icon-check {
+    flex-basis: 4rem;
+    flex-shrink: 0;
+    width: 4rem;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 2rem;
+
+    border: 2px solid #e6e6e6;
+    color: var(--primary);
+
+    transition: .3s ease-in-out;
+}
+.tab-contents-list ul li span {
+    flex: 1;
+    font-size: 1.5rem;
+}
+.tab-contents-img .tab-contents-img-half {
+    width: calc(50% - 1rem);
+    height: 37rem;
+    background: #000;
+}
+.tab-contents-img .tab-contents-img-half:first-child {
+    margin-right: 1rem;    
+}
+.tab-contents-img .tab-contents-img-half:last-child {
+    margin-left: 1rem;    
+}
+.tab-contents-img-half img{
+    display: block;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+}
+
+/* side contents */
+.tab-contents-side .rewards{
+    margin-bottom: 2rem;
+}
+
+
+/* reward */
+.rewards {
+    padding: 4rem;
+    background: #f6f3f1;
+}
+.rewards h4 {
+    font-size: 2.4rem;
+}
+.rewards-img {
+    height: 19.5rem;
+    margin: 2.5rem 0;
+    background: #000;
+}
+.rewards-img img {
+    display: block;
+    height: 100%;
+    width: 100%;
+    object-position: center;
+    object-fit: cover;
+}
+.rewards-info {
+    font-size: 1.8rem;
+}
+.rewards-info strong{
+    margin-bottom: 2rem;
+    color: var(--primary);
+    font-size: 2.4rem;
+    font-weight: 600;
+}
+.rewards p {
+    margin-bottom: 2.5rem;
+    font-size: 1.5rem;
+    line-height: 2.8rem;
+}
+.rewards-date {
+    font-size: 1.5rem;
+    line-height: 2.8rem;
+}
+.rewards-date h6 {
+    font-size: 1.8rem;
+    font-weight: 600;
+}
+.rewards-icon {
+    margin: 2.5rem 0;
+    font-size: 1.5rem;
+}
+.rewards-icon li {
+    margin-bottom: 2rem;
+}
+.rewards-icon i {
+    color: var(--primary);
+    margin-right: 1rem;
+}
+
+/* banner */
+.tab-contents-side {
+    --top: white;
+    --main: var(--secondary);
+    --bg: #2b3351;
+}
+.banner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 10rem 0;
+    text-align: center;
+    background: var(--bg);
+}
+
+.banner .banner-subtitle{
+    color: var(--top);
+    font-size: 1.8rem;
+    margin-bottom: 3rem;
+}
+.banner .banner-maintitle {
+    color: var(--main);
+    font-size: 3rem;
+    line-height: 3.5rem;
+    margin-bottom: 4rem;
+}
+
+/* responsive */
+@media screen and (max-width: 1024px) {
+    /* side responsive */
+    .tab-contents-side {
+        display: flex;
+        margin-top: 4rem;
+    }
+    .tab-contents-side .rewards {
+        flex: 5;
+        margin-right: 1rem;
+        margin-bottom: 0;
+    }
+
+    .tab-contents-side .banner {
+        flex: 5;
+        margin-left: 1rem;
+    }
+}
+
+@media screen and (max-width: 786px) {
+    /* main content */
+    
+    /* icon-list */
+    .tab-contents-list {
+        display: block;
+    }
+    .tab-contents-list ul {
+        width: 100%;
+        padding-right: 0rem;
+    }
+    /* img half */
+    .tab-contents-img .tab-contents-img-half {
+        width: 100%;
+        margin-bottom: 2rem;
+    }
+    .tab-contents-img .tab-contents-img-half:first-child {
+        margin-right: 0rem;    
+    }
+    .tab-contents-img .tab-contents-img-half:last-child {
+        margin: 0;
+    }
+    .tab-contents-img-half img{
+        display: block;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    /* side responsive */
+    .tab-contents-side {
+        display: block;
+    }
+    .tab-contents-side .rewards {
+        margin-right: 0;
+        margin-bottom: 0;
+        padding: 2rem;
+    }
+
+    .tab-contents-side .banner {
+        margin-left: 0rem;
+    }
+}
+
+
+</style>
