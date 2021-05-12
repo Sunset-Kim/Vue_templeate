1c0c5526db7f0d254a990f368a1bfb7e858be970
Sunset-Kim <wood2002@naver.com>
2021-05-12 09:42:54 +0900

    🚧 project단 작업중

---
+++ src/components/AppPhoto.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app-photo">
+        <div class="title-group">
+            <div class="title-top">Company History</div>
+            <div class="title-main">We Are Trusted Crowdfunding</div>
+            <div class="title-sub">Agency In Since 1990</div>
+        </div>
+
+        <div class="photo-container">
+            <div class="wrap">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="photo-item">
+                            <div class="photo-img">
+                                <img src="" alt="">
+                            </div>
+                            <div class="photo-text">
+                                <div class="label-year label-170 btn-primary">
+                                    <span>1990</span>
+                                </div>
+                                <p>
+                                    Sed perspicis unde omnis iste natus error sit voluptatem accusanti dolmque laudantium totam rem aperia
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app-photo {
+    padding: 13rem 0;
+}
+.photo-item {
+
+}
+
+
+.photo-img {
+    height: 37rem;
+    background: #000;
+}
+.photo-text {
+    text-align: center;
+    padding: 4rem 3.5rem;
+    background: white;
+}
+.photo-text p {
+    font-size: 1.5rem;
+    line-height: 2.8rem;
+    color: var(--text-sub);
+
+}
+</style>
--- src/pages/history/App.vue
+++ src/pages/history/App.vue
@@ -9,6 +9,7 @@
         <app-history-about></app-history-about>
         <app-subbanner></app-subbanner>
         <app-tabmenu></app-tabmenu>
+        <app-photo></app-photo>
         <app-contact></app-contact>
         <app-footer></app-footer>
     </div>
@@ -20,7 +21,7 @@ import AppSubmain from '../../components/AppSubmain'
 import AppHistoryAbout from '../../components/AppHistoryAbout'
 import AppSubbanner from '../../components/AppSubbanner'
 import AppTabmenu from '../../components/AppTabmenu'
-
+import AppPhoto from '../../components/AppPhoto'
 import AppContact from '../../components/AppContact'
 import AppFooter from '../../components/AppFooter'
 
@@ -32,6 +33,7 @@ export default {
         AppHistoryAbout,
         AppSubbanner,
         AppTabmenu,
+        AppPhoto,
         AppContact,
         AppFooter
     },
