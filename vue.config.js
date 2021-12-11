d35b234547ddc32fa4856a090836c37dbe211cee
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-12-11 22:35:24 +0900

    👷  배포용 dependency 추가 script 추가

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,10 @@
 module.exports = {
-    publicPath: '',
+    publicPath: '/',
     pages: {
         index: {
             entry: 'src/pages/home/main.js',
             template: 'public/index.html',
             filename: 'index.html',
-        },
-        about: {
-            entry: 'src/pages/about/main.js',
-            template: 'public/about.html',
-            filename: 'about.html',
-        },
-        history: {
-            entry: 'src/pages/history/main.js',
-            template: 'public/history.html',
-            filename: 'history.html',
-        },
-        project: {
-            entry: 'src/pages/project/main.js',
-            template: 'public/project.html',
-            filename: 'project.html',
-        },
-        project_img: {
-            entry: 'src/pages/project_img/main.js',
-            template: 'public/project_img.html',
-            filename: 'project_img.html',
-        },
+        }
     },
 }
